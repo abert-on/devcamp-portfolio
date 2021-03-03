45c0a4da86b4b5a268b661b3661625e84a7bc3ee
Alec Burton <alecburton@notonthehighstreet.com>
2021-03-03 10:08:04 +0000

    add name param to user registration and editing

--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2021_03_03_091156) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "DeviseCreateUsers"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
